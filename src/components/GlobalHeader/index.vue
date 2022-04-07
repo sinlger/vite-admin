9a136d77fd1a168458847fc48da8ea742c455dd7
Alpha <donghe587@139.com>
2022-04-07 15:47:24 +0800

    修改router

---
+++ src/components/GlobalHeader/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+import { useRoute, useRouter } from "vue-router";
+export default {
+  setup(props, context) {
+    const router = useRouter()
+    
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
